84f4c98af6c290a8c65e75550c7040cb6818e703
Sandeep Neupane <deep.newsun@gmail.com>
2020-07-30 02:08:27 -0400

    modified for better folder collection

--- organizer.py
+++ organizer.py
@@ -1,24 +1,30 @@
-import os,glob,shutil,sys
-extensionList = list()
+import os,glob,shutil,sys,requests,json
+extensionDict = requests.get(url = 'https://raw.githubusercontent.com/dyne/file-extension-list/master/pub/categories.json').json()
+
 path = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
-def folderGenerator():
-    for x in [f for f in os.listdir() if os.path.isfile(os.path.join(path, f))]:
-        filename,extension = os.path.splitext(x)
-        extension = extension[1:]
-        extensionList.append(extension) if extension not in extensionList and extension != '' else False
-    for folder in extensionList:
+def folderGeneratorCommon():
+    for folder in extensionDict.keys():
         folderDir = os.path.join(path,folder)
         if not os.path.exists(folderDir):
             os.makedirs(folderDir)
-            print('New folder created : ',folderDir)
-            yield folder,folderDir
-        else:
-            yield folder,folderDir
-                              
-for folder,folderDir in folderGenerator():
-    for file in glob.glob(os.path.join(path,'*.'+folder)):
+            print('Created folder : ',folderDir)
+        yield folder, extensionDict[folder]
+def organizeCommonFiles():
+    for folder,folderExtensions in folderGeneratorCommon():
+        for file in glob.glob(os.path.join(path,'*.'+folderExtensions)):
+            try:
+                shutil.move(file,os.path.join(path,folder)+ '\\')
+                print('Moved file : ',file)
+            except shutil.Error as err:
+                print(err)
+def organizeSpecialFiles():
+    specialFolder = os.path.join(path,'others')
+    os.makedirs(specialFolder) if not os.path.exists(specialFolder) else True
+    for file in [f for f in os.listdir() if os.path.isfile(os.path.join(path, f))]:
         try:
-            shutil.move(file,folderDir+ '\\')
+            shutil.move(os.path.join(path,file),specialFolder+'\\')
             print('Moved file : ',file)
         except shutil.Error as err:
             print(err)
+organizeCommonFiles()
+organizeSpecialFiles()
